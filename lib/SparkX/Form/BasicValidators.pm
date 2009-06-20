528688a44c16506d3f4418db33c899535e85d413
James Laver <jjl@baozi.local>
2009-06-20 10:38:28 +0100

    Initial commit, to share with laptop

---
+++ lib/SparkX/Form/BasicValidators.pm
@@ -0,0 +1,107 @@
+package SparkX::Form::BasicValidators;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+SparkX::Form::BasicValidators - The great new SparkX::Form::BasicValidators!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use SparkX::Form::BasicValidators;
+
+    my $foo = SparkX::Form::BasicValidators->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+James Laver, C<< <printf(qw[%s at %s.%s cpan jameslaver com])> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-sparkx-form-basicvalidators at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=SparkX-Form-BasicValidators>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc SparkX::Form::BasicValidators
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=SparkX-Form-BasicValidators>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/SparkX-Form-BasicValidators>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/SparkX-Form-BasicValidators>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/SparkX-Form-BasicValidators/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 James Laver, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of SparkX::Form::BasicValidators
---
+++ t/00-load.t
@@ -0,0 +1,9 @@
+#!perl -T
+
+use Test::More tests => 1;
+
+BEGIN {
+	use_ok( 'SparkX::Form::BasicValidators' );
+}
+
+diag( "Testing SparkX::Form::BasicValidators $SparkX::Form::BasicValidators::VERSION, Perl $], $^X" );
