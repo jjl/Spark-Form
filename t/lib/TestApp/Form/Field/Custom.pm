dd98010ec37f6abe95f08016e1e0e2fd556ec85f
James <jjl@jjl.(none)>
2009-06-26 19:48:14 +0100

    Fixed custom test field

--- t/lib/TestApp/Form/Field/Custom.pm
+++ t/lib/TestApp/Form/Field/Custom.pm
@@ -2,21 +2,20 @@ package TestApp::Form::Field::Custom;
 
 use Moose;
 extends 'Spark::Form::Field';
-with 'Spark::Form::Field::Validator::MinLength';
+with 'Spark::Form::Field::Role::Validateable';
 
-has '+min_length' => (
+has 'min_length' => (
+    isa => 'Int',
+    is => 'rw',
     required => 0,
     default  => 6,
 );
 
-has '+errmsg_too_short' => (
-    default  => "Failed"
-);
-
 sub validate {
     my ($self) = @_;
-    #This will be overridden by ::Role::MinLength if necessary.
-    $self->valid(1);
+    if ($self->min_length > length $self->value) {
+        $self->error('Customs must be at least ' . $self->min_length . ' characters long.');
+    }
 }
 
 1;
