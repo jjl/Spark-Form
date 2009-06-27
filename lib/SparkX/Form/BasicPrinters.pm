65febc955578a37b3d7fc0503f63dd6a0a077911
James <jjl@jjl.(none)>
2009-06-27 17:32:11 +0100

    Added BasicPrinters.pm so people have somewhere to look

---
+++ lib/SparkX/Form/BasicPrinters.pm
@@ -0,0 +1,19 @@
+package SparkX::Form::BasicPrinters;
+
+our $VERSION="0.1";
+
+
+1;
+__END__
+
+=head1 NAME
+
+SparkX::Form::BasicPrinters - A selection of basic form printers for printing a C<SparkX::Form>.
+
+=head1 SEE ALSO
+
+L<SparkX::Form> - The forms module that started it all
+L<SparkX::Form::Printer> - The interface that printers must fulfil
+L<SparkX::Form::Printer::List> - A printer that prints a HTML list
+
+=cut
