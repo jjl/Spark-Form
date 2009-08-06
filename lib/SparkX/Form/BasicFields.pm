da9fef591872e35d12005797ba5926bcdd80314c
Kent Fredric <kentfredric@gmail.com>
2009-08-07 07:49:55 +1200

    Nuking references to gone dists

--- lib/SparkX/Form/BasicFields.pm
+++ lib/SparkX/Form/BasicFields.pm
@@ -21,26 +21,6 @@ L<SparkX::Form::Field::Radio> - A radio button set
 L<SparkX::Form::Field::Select> - A select button set
 L<SparkX::Form::Field::TextArea> - A textarea field
 
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-sparkx-form-basicfields at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=SparkX-Form-BasicFields>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-=head1 SUPPORT
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=SparkX-Form-BasicFields>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/SparkX-Form-BasicFields>
-
-=back
-
 =head1 ACKNOWLEDGEMENTS
 
 =cut
--- lib/SparkX/Form/BasicValidators.pm
+++ lib/SparkX/Form/BasicValidators.pm
@@ -13,24 +13,4 @@ __END__
 L<Spark::Form> - The forms module that started it all
 L<Spark::Form::Manual> - Walkthrough
 
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-sparkx-form-basicvalidators at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=SparkX-Form-BasicValidators>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-=head1 SUPPORT
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=SparkX-Form-BasicValidators>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/SparkX-Form-BasicValidators>
-
-=back
-
 =cut
