da28b5df8f264bce9b0e4a27b29fddd015042b45
Kent Fredric <kentfredric@gmail.com>
2009-09-13 10:31:33 +1200

    Bumped the dep to DC 0.02004422 and use its feature

--- lib/Spark/Form.pm
+++ lib/Spark/Form.pm
@@ -322,7 +322,7 @@ sub clone_except_ids {
 
 sub clone_only_ids {
     my ($self, @ids) = @_;
-    my @all = 0 ... ( $self->_fields->count - 1 );
+    my @all = 0 .. $self->_fields->last_id;
 
     return $self->clone_except_ids($self->_except(\@all, \@ids));
 }
