20021f84310d04a64509e7d9d78dbd51946a7209
James Laver <james@jameslaver.com>
2011-03-27 17:04:26 +0100

    Made RadioGroup use a Spark::Couplet so we can maintain order and associate text with values

--- lib/SparkX/Form/Field/RadioGroup.pm
+++ lib/SparkX/Form/Field/RadioGroup.pm
@@ -4,7 +4,7 @@ package SparkX::Form::Field::RadioGroup;
 
 use Moose;
 use HTML::Tiny;
-
+use Spark::Types qw(SCouplet);
 extends 'Spark::Form::Field';
 with 'Spark::Form::Field::Role::Printable::HTML',
   'Spark::Form::Field::Role::Printable::XHTML';
@@ -14,11 +14,15 @@ has '+value' => (
 );
 
 has options => (
-    isa      => 'ArrayRef',
+    isa      => SCouplet,
     is       => 'rw',
+    coerce => 1,
     required => 0,
     lazy     => 1,
     default  => sub { return shift->value },
+    handles => {
+        options_kv => 'key_values_paired',
+    },
 );
 
 sub to_html {
@@ -30,12 +34,12 @@ sub to_xhtml {
 }
 
 sub _render_element {
-    my ($self, $html, $option, $count) = @_;
+    my ($self, $html, $name, $value, $count) = @_;
     my $id = join('-' => ($self->name, $count));
-    my $label = $html->label({for => $id});
+    my $label = $html->label({for => $id}, $name);
     my $input = $html->input({
         type => 'radio',
-        value => $option,
+        value => $value,
         id => $id,
     });
     return join("\n" => ($label, $input));
@@ -44,7 +48,13 @@ sub _render_element {
 sub _render {
     my ($self, $html) = @_;
     my $count; # We use this to generate unique IDs for the labels
-    my @options = map { $self->_render_element($html, $_, ++$count) } @{$self->options};
+    my @options = map {
+        $self->_render_element(
+            $html, # HTML::Tiny
+            @{$_}, # Key, Value strings
+            ++$count, # Uniquifier
+        );
+    } $self->options_kv;
     return join "\n",@options;
 }
 __PACKAGE__->meta->make_immutable;
@@ -54,6 +64,8 @@ __END__
 =head1 ATTRIBUTES
 
 =head2 options => L<Spark::Couplet>
+
+Text => value pairs. Coerces from ArrayRef with regular list inside
 
 =head1 METHODS
 
--- t/30-radiogroup.t
+++ t/30-radiogroup.t
@@ -8,14 +8,18 @@ use SparkX::Form::Field::RadioGroup;
 my $form = Spark::Form->new;
 my $b = SparkX::Form::Field::RadioGroup->new(
     name => 'test', form => $form,
-    options => ['test-1','test-2','test-3'],
+    options => [
+        'Radio Button 1', => 'r1',
+        'Radio Button 2', => 'r2',
+        'Radio Button 3', => 'r3',
+    ],
     value => 'test-2',
 );
 
 is($b->to_xhtml,
-   '<label for="test-1"></label>
-<input id="test-1" type="radio" value="test-1" />
-<label for="test-2"></label>
-<input id="test-2" type="radio" value="test-2" />
-<label for="test-3"></label>
-<input id="test-3" type="radio" value="test-3" />','Test xhtml representation');
+   '<label for="test-1">Radio Button 1</label>
+<input id="test-1" type="radio" value="r1" />
+<label for="test-2">Radio Button 2</label>
+<input id="test-2" type="radio" value="r2" />
+<label for="test-3">Radio Button 3</label>
+<input id="test-3" type="radio" value="r3" />','Test xhtml representation');
