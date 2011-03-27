b940235c60c9b728964f750077edbcd085ad3039
James Laver <james@jameslaver.com>
2011-03-27 17:04:26 +0100

    Made RadioGroup use a Spark::Couplet so we can maintain order and associate text with values

--- lib/Spark/Types.pm
+++ lib/Spark/Types.pm
@@ -8,7 +8,15 @@ use MooseX::Types
         SFieldValidator
     )];
 
+use MooseX::Types::Moose qw(ArrayRef);
+
+use Spark::Couplet;
+
 class_type SCouplet, { class => 'Spark::Couplet' };
+coerce SCouplet,
+    from ArrayRef,
+    via { Spark::Couplet->new(@$_) };
+
 class_type SForm, { class => 'Spark::Form' };
 class_type SField, { class => 'Spark::Form::Field' };
 class_type SFieldValidator, { class => 'Spark::Form::Field::Validator' };
