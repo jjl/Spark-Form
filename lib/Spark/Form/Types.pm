892482cefadc5476d89e0eff08a97367e93773e7
Kent Fredric <kentfredric@gmail.com>
2011-06-13 15:59:29 +1200

    Tidy Type classes and make critic not complain

--- lib/Spark/Form/Types.pm
+++ lib/Spark/Form/Types.pm
@@ -7,17 +7,17 @@ package Spark::Form::Types;
 
 use MooseX::Types::Moose qw(:all);
 use MooseX::Types -declare => [
-  qw(
-    PluginNamespaceList
-    SparkFormField
-    SparkForm
-    LabelledObject
-    NamedObject
-    SCouplet
-    SForm
-    SField
-    SFieldValidator
-    )
+    qw(
+      PluginNamespaceList
+      SparkFormField
+      SparkForm
+      LabelledObject
+      NamedObject
+      SCouplet
+      SForm
+      SField
+      SFieldValidator
+      )
 ];
 
 subtype PluginNamespaceList, as ArrayRef [Str];
@@ -26,21 +26,21 @@ coerce PluginNamespaceList, from Str, via { [$_] };
 
 coerce PluginNamespaceList, from Undef, via { [] };
 
-class_type SparkFormField, { class => 'Spark::Form::Field' };
+class_type SparkFormField, {class => 'Spark::Form::Field'};
 
-class_type SparkForm, { class => 'Spark::Form' };
+class_type SparkForm, {class => 'Spark::Form'};
 
 subtype LabelledObject, as Object, where {
-  $_->can('label') and $_->label;
+    $_->can('label') and $_->label;
 };
 
 subtype NamedObject, as Object, where {
-  $_->can('name') and $_->name;
+    $_->can('name') and $_->name;
 };
-class_type SCouplet, { class => 'Spark::Couplet' };
-class_type SForm, { class => 'Spark::Form' };
-class_type SField, { class => 'Spark::Form::Field' };
-class_type SFieldValidator, { class => 'Spark::Form::Field::Validator' };
+class_type SCouplet,        {class => 'Spark::Couplet'};
+class_type SForm,           {class => 'Spark::Form'};
+class_type SField,          {class => 'Spark::Form::Field'};
+class_type SFieldValidator, {class => 'Spark::Form::Field::Validator'};
 
 1;
 __END__
--- lib/Spark/Types.pm
+++ lib/Spark/Types.pm
@@ -1,25 +1,25 @@
 package Spark::Types;
 
 use MooseX::Types
-    -declare => [ qw(
-        SCouplet
-        SForm
-        SField
-        SFieldValidator
-    )];
+  -declare => [qw(
+      SCouplet
+      SForm
+      SField
+      SFieldValidator
+      )];
 
 use MooseX::Types::Moose qw(ArrayRef);
 
 use Spark::Couplet;
 
-class_type SCouplet, { class => 'Spark::Couplet' };
+class_type SCouplet, {class => 'Spark::Couplet'};
 coerce SCouplet,
-    from ArrayRef,
-    via { Spark::Couplet->new(@$_) };
+  from ArrayRef,
+  via { Spark::Couplet->new(@{$_}) };
 
-class_type SForm, { class => 'Spark::Form' };
-class_type SField, { class => 'Spark::Form::Field' };
-class_type SFieldValidator, { class => 'Spark::Form::Field::Validator' };
+class_type SForm,           {class => 'Spark::Form'};
+class_type SField,          {class => 'Spark::Form::Field'};
+class_type SFieldValidator, {class => 'Spark::Form::Field::Validator'};
 
 1;
 __END__
