b09752816b10ffd96ddf580e22f33aa0ce7e0dcf
Kent Fredric <kentfredric@gmail.com>
2011-06-13 16:04:52 +1200

    Tidy/Fix Critic concerns in ArrayRef plugin

--- lib/Spark/Couplet/Plugin/ArrayRef.pm
+++ lib/Spark/Couplet/Plugin/ArrayRef.pm
@@ -4,14 +4,14 @@ use Moose::Role;
 with 'Data::Couplet::Role::Plugin';
 
 sub add_arrayref {
-    my ($self,$key,$value) = @_;
+    my ($self, $key, $value) = @_;
     my $v = $self->value($key) || [];
-    $self->set($key, [@$v,$value]);
+    return $self->set($key, [@{$v}, $value]);
 }
 
 sub get_arrayref {
-    my ($self,$key) = @_;
-    @{$self->value($key)||[]};
+    my ($self, $key) = @_;
+    return @{$self->value($key) || []};
 }
 
 no Moose::Role;
