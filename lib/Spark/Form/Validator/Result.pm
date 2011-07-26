45bda589161f3932cc0c70a519ba567f5c7fabb2
James Laver <james@jameslaver.com>
2011-07-26 23:22:35 +0100

    Added Form validator objects

---
+++ lib/Spark/Form/Validator/Result.pm
@@ -0,0 +1,19 @@
+package Spark::Form::Validator::Result;
+
+use Moose;
+
+has bool => (
+    isa      => 'Bool',
+    is       => 'ro',
+    required => 1,
+);
+
+has message => (
+    isa     => 'Str',
+    is      => 'ro',
+    default => q{},
+);
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
+__END__
